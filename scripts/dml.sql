c685d85908a41633ed26d65cc1e6f132643e7141
Ale241302 <jorjecasanova@gmail.com>
2026-03-24 11:17:08 -0500

    chore: add DDL and DML scripts for database setup

---
+++ scripts/dml.sql
@@ -0,0 +1,17 @@
+USE prueba_tecnica_jorge;
+
+-- Datos precargados para PRO_PROCESO
+INSERT INTO PRO_PROCESO (PRO_NOMBRE, PRO_PREFIJO) VALUES 
+('Ingeniería', 'ING'),
+('Recursos Humanos', 'RH'),
+('Finanzas', 'FIN'),
+('Tecnología', 'TEC'),
+('Calidad', 'CAL');
+
+-- Datos precargados para TIP_TIPO_DOC
+INSERT INTO TIP_TIPO_DOC (TIP_NOMBRE, TIP_PREFIJO) VALUES 
+('Instructivo', 'INS'),
+('Procedimiento', 'PRO'),
+('Manual', 'MAN'),
+('Formato', 'FOR'),
+('Política', 'POL');
